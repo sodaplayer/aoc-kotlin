9b1724e31e2f1202b2ee9d3b94d6a62e22133d4a
John Fresco <john@johnfres.co>
2021-12-11 01:32:07 -0700

    Add some essential util functions

--- src/main/kotlin/aoc2021/utils/utils.kt
+++ src/main/kotlin/aoc2021/utils/utils.kt
@@ -6,6 +6,8 @@ fun loadInput(path: String): InputStream {
     return checkNotNull({}::class.java.getResourceAsStream(path))
 }
 
+fun <T> Iterable<T>.head(): T = this.first()
 fun <T> Iterable<T>.tail(): Iterable<T> = this.drop(1)
 
-fun <T> Iterable<T>.head(): T = this.first()
+fun <T> Iterable<T>.car(): T = this.first()
+fun <T> Iterable<T>.cdr(): Iterable<T> = this.drop(1)
